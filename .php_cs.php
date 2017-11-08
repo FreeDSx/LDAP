bd86fdad4ee7777c4e109818d0dad9efadab416c
Chad Sikorra <chad.sikorra@gmail.com>
2017-11-08 12:43:07 -0600

    Remove an unused file.

--- .php_cs.php
+++
@@ -1,9 +0,0 @@
-<?php
-$finder = Symfony\CS\Finder\DefaultFinder::create()
-    ->exclude('tests')
-    ->in('src')
-;
-return Symfony\CS\Config\Config::create()
-    ->finder($finder)
-    ->level(Symfony\CS\FixerInterface::PSR2_LEVEL)
-    ;
