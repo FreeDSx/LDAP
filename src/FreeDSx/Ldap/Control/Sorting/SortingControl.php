ac406dec99f46e17686541e3e26fb076a8d0b00e
Chad Sikorra <Chad.Sikorra@gmail.com>
2017-12-12 08:13:54 -0600

    Parse sorting keys from ASN1.

--- src/FreeDSx/Ldap/Control/Sorting/SortingControl.php
+++ src/FreeDSx/Ldap/Control/Sorting/SortingControl.php
@@ -11,8 +11,13 @@
 namespace FreeDSx\Ldap\Control\Sorting;
 
 use FreeDSx\Ldap\Asn1\Asn1;
+use FreeDSx\Ldap\Asn1\Encoder\BerEncoder;
 use FreeDSx\Ldap\Asn1\Type\AbstractType;
+use FreeDSx\Ldap\Asn1\Type\IncompleteType;
+use FreeDSx\Ldap\Asn1\Type\OctetStringType;
+use FreeDSx\Ldap\Asn1\Type\SequenceType;
 use FreeDSx\Ldap\Control\Control;
+use FreeDSx\Ldap\Exception\ProtocolException;
 
 /**
  * A Server Side Sorting request control value. RFC 2891.
@@ -77,7 +82,45 @@ class SortingControl extends Control
      */
     public static function fromAsn1(AbstractType $type)
     {
-        // TODO: Implement fromAsn1() method.
+        $response = self::decodeEncodedValue($type);
+        if (!$response instanceof SequenceType) {
+            throw new ProtocolException('The sorting control is malformed.');
+        }
+
+        $sortKeys = [];
+        /** @var SequenceType $response */
+        foreach ($response as $sequence) {
+            if (!$response instanceof SequenceType) {
+                throw new ProtocolException('The sort key is malformed.');
+            }
+
+            $attrName = null;
+            $matchRule = null;
+            $useReverseOrder = false;
+
+            $encoder = new BerEncoder();
+            /** @var AbstractType $keyItem */
+            foreach ($sequence as $keyItem) {
+                if ($keyItem instanceof OctetStringType && $keyItem->getTagClass() === AbstractType::TAG_CLASS_UNIVERSAL) {
+                    $attrName = $keyItem->getValue();
+                } elseif ($keyItem->getTagClass() === AbstractType::TAG_CLASS_CONTEXT_SPECIFIC && $keyItem->getTagNumber() === 0) {
+                    $matchRule = $keyItem->getValue();
+                } elseif ($keyItem->getTagClass() === AbstractType::TAG_CLASS_CONTEXT_SPECIFIC && $keyItem->getTagNumber() === 1) {
+                    /** @var IncompleteType $keyItem */
+                    $useReverseOrder = $encoder->complete($keyItem, AbstractType::TAG_TYPE_BOOLEAN)->getValue();
+                } else {
+                    throw new ProtocolException('The sorting control contains unexpected data.');
+                }
+            }
+            if (empty($attrName) && $attrName !== '0') {
+                throw new ProtocolException('The sort key is missing an attribute description.');
+            }
+
+            $sortKeys[] = new SortKey($attrName, $useReverseOrder, $matchRule);
+        }
+        $control = new self(...$sortKeys);
+
+        return self::mergeControlData($control, $type);
     }
 
     /**
--- tests/spec/FreeDSx/Ldap/Control/Sorting/SortingControlSpec.php
+++ tests/spec/FreeDSx/Ldap/Control/Sorting/SortingControlSpec.php
@@ -15,6 +15,7 @@ use FreeDSx\Ldap\Asn1\Encoder\BerEncoder;
 use FreeDSx\Ldap\Control\Control;
 use FreeDSx\Ldap\Control\Sorting\SortingControl;
 use FreeDSx\Ldap\Control\Sorting\SortKey;
+use FreeDSx\Ldap\Exception\ProtocolException;
 use PhpSpec\ObjectBehavior;
 
 class SortingControlSpec extends ObjectBehavior
@@ -79,6 +80,47 @@ class SortingControlSpec extends ObjectBehavior
 
     function it_should_be_constructed_from_asn1()
     {
+        $encoder = new BerEncoder();
+        $this::fromAsn1(Asn1::sequence(
+            Asn1::ldapOid(Control::OID_SORTING),
+            Asn1::boolean(false),
+            Asn1::octetString($encoder->encode(Asn1::sequenceOf(
+                Asn1::sequence(Asn1::octetString('foo')),
+                Asn1::sequence(Asn1::octetString('bar')),
+                Asn1::sequence(
+                    Asn1::octetString('foobar'),
+                    Asn1::context(0, Asn1::ldapString('bleh')),
+                    Asn1::context(1, Asn1::boolean(true))
+                )
+            )))
+        ))->setValue(null)->shouldBeLike(new SortingControl(
+            new SortKey('foo'),
+            new SortKey('bar'),
+            new SortKey('foobar', true, 'bleh')
+        ));
+    }
 
+    function it_should_throw_an_error_parsing_sorting_keys_with_no_attribute()
+    {
+        $encoder = new BerEncoder();
+        $this->shouldThrow(ProtocolException::class)->during('fromAsn1', [Asn1::sequence(
+            Asn1::ldapOid(Control::OID_SORTING),
+            Asn1::boolean(false),
+            Asn1::octetString($encoder->encode(Asn1::sequenceOf(
+                Asn1::sequence(Asn1::octetString(''))
+            )))
+        )]);
+    }
+
+    function it_should_throw_an_error_parsing_sorting_keys_with_unexpected_values()
+    {
+        $encoder = new BerEncoder();
+        $this->shouldThrow(ProtocolException::class)->during('fromAsn1', [Asn1::sequence(
+            Asn1::ldapOid(Control::OID_SORTING),
+            Asn1::boolean(false),
+            Asn1::octetString($encoder->encode(Asn1::sequenceOf(
+                Asn1::sequence(Asn1::octetString('foo'), Asn1::enumerated(1))
+            )))
+        )]);
     }
 }
