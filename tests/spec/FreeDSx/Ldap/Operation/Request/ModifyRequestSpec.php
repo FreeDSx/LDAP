71bdbbb393f9be9ef8326be2eb0063ecd1c59048
Chad Sikorra <Chad.Sikorra@gmail.com>
2017-12-13 08:26:01 -0600

    Add specs for modify request invalid ASN1 handling.

--- tests/spec/FreeDSx/Ldap/Operation/Request/ModifyRequestSpec.php
+++ tests/spec/FreeDSx/Ldap/Operation/Request/ModifyRequestSpec.php
@@ -6,6 +6,7 @@ use FreeDSx\Ldap\Asn1\Asn1;
 use FreeDSx\Ldap\Entry\Change;
 use FreeDSx\Ldap\Entry\Dn;
 use FreeDSx\Ldap\Entry\Entry;
+use FreeDSx\Ldap\Exception\ProtocolException;
 use FreeDSx\Ldap\Operation\Request\ModifyRequest;
 use PhpSpec\ObjectBehavior;
 
@@ -83,13 +84,79 @@ class ModifyRequestSpec extends ObjectBehavior
         $this::fromAsn1($req->toAsn1())->shouldBeLike($req);
     }
 
-    function it_should_not_be_constructed_asn1_with_invalid_changes()
+    function it_should_not_be_constructed_from_asn1_with_an_invalid_dn_type()
     {
-
+        $this->shouldThrow(ProtocolException::class)->during('fromAsn1', [Asn1::sequence(
+            Asn1::integer(1),
+            Asn1::sequence()
+        )]);
     }
 
-    function it_should_not_be_constructed_asn1_with_invalid_partial_attributes()
+    function it_should_not_be_constructed_from_asn1_with_an_invalid_changelist()
     {
+        $this->shouldThrow(ProtocolException::class)->during('fromAsn1', [Asn1::set(
+            Asn1::octetString('dc=foo'),
+            Asn1::sequence()
+        )]);
+    }
 
+    function it_should_not_be_constructed_from_asn1_with_an_invalid_changelist_type()
+    {
+        $this->shouldThrow(ProtocolException::class)->during('fromAsn1', [Asn1::sequence(
+            Asn1::integer(1),
+            Asn1::sequence()
+        )]);
+    }
+
+    function it_should_not_be_constructed_from_asn1_with_invalid_attribute_values()
+    {
+        $this->shouldThrow(ProtocolException::class)->during('fromAsn1', [Asn1::sequence(
+            Asn1::octetString('foo'),
+            Asn1::sequence(
+                Asn1::sequence(
+                    Asn1::enumerated(2),
+                    Asn1::sequence(
+                        Asn1::octetString('foo'),
+                        Asn1::sequence(
+                            Asn1::octetString('bar')
+                        )
+                    )
+                )
+            )
+        )]);
+    }
+
+    function it_should_not_be_constructed_from_asn1_without_a_partial_attribute_description()
+    {
+        $this->shouldThrow(ProtocolException::class)->during('fromAsn1', [Asn1::sequence(
+            Asn1::octetString('foo'),
+            Asn1::sequence(
+                Asn1::sequence(
+                    Asn1::enumerated(2),
+                    Asn1::sequence(
+                        Asn1::setOf(
+                            Asn1::octetString('bar')
+                        )
+                    )
+                )
+            )
+        )]);
+    }
+
+    function it_should_not_be_constructed_from_asn1_without_a_change_type()
+    {
+        $this->shouldThrow(ProtocolException::class)->during('fromAsn1', [Asn1::sequence(
+            Asn1::octetString('foo'),
+            Asn1::sequence(
+                Asn1::sequence(
+                    Asn1::integer(999),
+                    Asn1::sequence(
+                        Asn1::setOf(
+                            Asn1::octetString('bar')
+                        )
+                    )
+                )
+            )
+        )]);
     }
 }
