b5a3f818cef8ac23c73d4bd9b52b18ec0b6bb2e5
Chad Sikorra <chad.sikorra@gmail.com>
2019-02-16 14:10:35 -0600

    Add unit tests using an OpenLDAP server with test data. (#14)

---
+++ tests/resources/openldap/setup.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+
+# Adapted from the ruby-net-ldap setup scripts:
+#     https://github.com/ruby-ldap/ruby-net-ldap
+#
+# Which seemed to originate from github-ldap integration scripts:
+#     https://github.com/github/github-ldap
+#
+# Adapted for the needs of this library.
+
+set -e
+set -x
+
+service slapd stop
+
+# Remove existing data, reconfigure...
+cp -v /var/lib/ldap/DB_CONFIG ./DB_CONFIG
+rm -rf /etc/ldap/slapd.d/*
+rm -rf /var/lib/ldap/*
+cp -v ./DB_CONFIG /var/lib/ldap/DB_CONFIG
+
+# Some initial imports...
+slapadd -F /etc/ldap/slapd.d -b "cn=config" -l ./tests/resources/openldap/conf.ldif
+slapadd -F /etc/ldap/slapd.d -b "cn=config" -l ./tests/resources/openldap/memberof.ldif
+
+# Add the base...
+slapadd -F /etc/ldap/slapd.d <<EOM
+dn: dc=example,dc=com
+objectClass: top
+objectClass: domain
+dc: example
+EOM
+
+chown -R openldap.openldap /etc/ldap/slapd.d
+chown -R openldap.openldap /var/lib/ldap
+
+service slapd start
+
+# Import the test data ...
+/usr/bin/time ldapadd -x -D "cn=admin,dc=example,dc=com" -w 12345 -h localhost -p 389 -f ./tests/resources/openldap/data.ldif > /dev/null
+
+# Generate the CA cert
+certtool --generate-privkey > /etc/ssl/private/example.key
+certtool --generate-self-signed \
+  --load-privkey /etc/ssl/private/example.key \
+  --template ./tests/resources/openldap/ca.info \
+  --outfile /usr/local/share/ca-certificates/example.crt
+
+update-ca-certificates
+
+# Generate the actual cert used by slapd...
+certtool --generate-privkey \
+  --bits 2048 \
+  --outfile /etc/ssl/private/slapd.key
+certtool --generate-certificate \
+  --load-privkey /etc/ssl/private/slapd.key \
+  --load-ca-certificate /usr/local/share/ca-certificates/example.crt \
+  --load-ca-privkey /etc/ssl/private/example.key \
+  --template ./tests/resources/openldap/slapd.info \
+  --outfile /etc/ssl/certs/slapd.crt
+
+# Need to modify the config to actually use the generated cert...
+ldapmodify -Y EXTERNAL -H ldapi:/// <<EOF | true
+dn: cn=config
+add: olcTLSCACertificateFile
+olcTLSCACertificateFile: /usr/local/share/ca-certificates/example.crt
+-
+add: olcTLSCertificateFile
+olcTLSCertificateFile: /etc/ssl/certs/slapd.crt
+-
+add: olcTLSCertificateKeyFile
+olcTLSCertificateKeyFile: /etc/ssl/private/slapd.key
+EOF
+
+# Used to enable "ldaps://" (never standardized from an RFC like StartTLS, though still commonly used) ...
+sed -i -e 's|^SLAPD_SERVICES="\(.*\)"|SLAPD_SERVICES="ldap:/// ldapi:/// ldaps:///"|' /etc/default/slapd
+
+# Needed permission / user changes ...
+adduser openldap ssl-cert
+chgrp ssl-cert /etc/ssl/private/slapd.key
+chmod g+r /etc/ssl/private/slapd.key
+chmod o-r /etc/ssl/private/slapd.key
+
+# Needed so we can access LDAP via the proper name in the cert, final one to test a failure...
+echo "127.0.0.1 ldap.example.com" >> /etc/hosts
+echo "127.0.0.1 example.com" >> /etc/hosts
+echo "127.0.0.1 ldap.foo.com" >> /etc/hosts
+
+service slapd restart
---
+++ tests/unit/FreeDSx/Ldap/LdapClientTest.php
@@ -0,0 +1,196 @@
+<?php
+/**
+ * This file is part of the FreeDSx LDAP package.
+ *
+ * (c) Chad Sikorra <Chad.Sikorra@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace unit\FreeDSx\Ldap;
+
+use FreeDSx\Ldap\Entry\Entries;
+use FreeDSx\Ldap\Entry\Entry;
+use FreeDSx\Ldap\Exception\ConnectionException;
+use FreeDSx\Ldap\LdapClient;
+use FreeDSx\Ldap\Operation\Response\AddResponse;
+use FreeDSx\Ldap\Operation\Response\BindResponse;
+use FreeDSx\Ldap\Operation\Response\DeleteResponse;
+use FreeDSx\Ldap\Operation\Response\ModifyResponse;
+use FreeDSx\Ldap\Operations;
+use FreeDSx\Ldap\Search\Filters;
+
+class LdapClientTest extends LdapTestCase
+{
+    /**
+     * @var LdapClient
+     */
+    protected $client;
+
+    protected function setUp()
+    {
+        $this->client = $this->getClient();
+    }
+
+    protected function tearDown()
+    {
+        $this->client->unbind();
+    }
+
+    public function testUsernamePasswordBind()
+    {
+        $response = $this->client->bind($_ENV['LDAP_USERNAME'], $_ENV['LDAP_PASSWORD'])->getResponse();
+
+        $this->assertInstanceOf(BindResponse::class, $response);
+        $this->assertEquals(0, $response->getResultCode());
+    }
+
+    public function testAnonymousBind()
+    {
+        $response = $this->client->send(Operations::bindAnonymously())->getResponse();
+
+        $this->assertInstanceOf(BindResponse::class, $response);
+        $this->assertEquals(0, $response->getResultCode());
+    }
+
+    public function testCompareOperation()
+    {
+        $this->bindClient($this->client);
+
+        $success = $this->client->compare('cn=Birgit Pankhurst,ou=Janitorial,dc=example,dc=com', 'cn', 'Birgit Pankhurst');
+        $failure = $this->client->compare('cn=Birgit Pankhurst,ou=Janitorial,dc=example,dc=com', 'cn', 'foo');
+
+        $this->assertTrue($success);
+        $this->assertFalse($failure);
+    }
+
+    public function testCreateOperation()
+    {
+        $this->bindClient($this->client);
+
+        $attributes = [
+            'objectClass' => ['top', 'person', 'organizationalPerson', 'inetOrgPerson'],
+            'cn' => ['Foo'],
+            'sn' => ['Bar'],
+            'description' => ['FreeDSx Unit Test'],
+            'userPassword' => ['FreeDSx-Unit-Test'],
+            'uid' => ['Foo'],
+            'givenName' => ['Foo'],
+        ];
+
+        $response = $this->client->create(Entry::fromArray('cn=Foo,dc=example,dc=com', $attributes))->getResponse();
+        $this->assertInstanceOf(AddResponse::class, $response);
+        $this->assertEquals(0, $response->getResultCode());
+
+        $entry = $this->client->read('cn=Foo,dc=example,dc=com', array_keys($attributes));
+        $this->assertEquals($attributes, $entry->toArray());
+    }
+
+    public function testReadOperation()
+    {
+        $this->bindClient($this->client);
+
+        $attributes = [
+            'objectClass' => ['top', 'person', 'organizationalPerson', 'inetOrgPerson'],
+            'cn' => ['Carmelina Esposito'],
+            'sn' => ['Esposito'],
+            'description' =>  ["This is Carmelina Esposito's description"],
+            'facsimileTelephoneNumber' => ['+1 415 116-9439'],
+            'l' => ['San Jose'],
+            'postalAddress' => ['Product Testing$San Jose'],
+            'manager' => ['cn=Ria Corace,ou=Product Development,dc=example,dc=com'],
+            'secretary' => ['cn=Melhem Donak,ou=Administrative,dc=example,dc=com'],
+        ];
+        $entry = $this->client->read('cn=Carmelina Esposito,ou=Product Testing,dc=example,dc=com', array_keys($attributes));
+
+        $this->assertInstanceOf(Entry::class, $entry);
+        $this->assertEquals($entry->getDn()->toString(), 'cn=Carmelina Esposito,ou=Product Testing,dc=example,dc=com');
+        $this->assertEquals($entry->toArray(), $attributes);
+    }
+
+    public function testDeleteOperation()
+    {
+        $this->bindClient($this->client);
+
+        $response = $this->client->delete('cn=Foo,dc=example,dc=com')->getResponse();
+        $this->assertInstanceOf(DeleteResponse::class, $response);
+        $this->assertEquals(0, $response->getResultCode());
+    }
+
+    public function testModifyOperation()
+    {
+        $this->bindClient($this->client);
+
+        $entry = new Entry('cn=Kathrine Erbach,ou=Payroll,dc=example,dc=com');
+        $entry->reset('facsimileTelephoneNumber');
+        $entry->add('mobile', '+1 555 555-5555', '+1 666 666-6666');
+        $entry->remove('homePhone', '+1 510 991-4348');
+        $entry->set('title', 'Head Payroll Dude');
+
+        $response = $this->client->update($entry)->getResponse();
+        $this->assertInstanceOf(ModifyResponse::class, $response);
+        $this->assertEquals(0, $response->getResultCode());
+        $this->assertEmpty($entry->changes()->toArray());
+
+        $modified = $this->client->read('cn=Kathrine Erbach,ou=Payroll,dc=example,dc=com', [
+            'facsimileTelephoneNumber',
+            'mobile',
+            'homePhone',
+            'title',
+        ]);
+        $this->assertEquals(['mobile' => ['+1 510 957-7341', '+1 555 555-5555', '+1 666 666-6666'], 'title' => ['Head Payroll Dude']], $modified->toArray());
+    }
+
+    public function testSubSearchOperation()
+    {
+        $this->bindClient($this->client);
+
+        $entries = $this->client->search(Operations::search(Filters::raw('(&(objectClass=inetOrgPerson)(cn=A*))')));
+        $this->assertInstanceOf(Entries::class, $entries);
+        $this->assertEquals(843, $entries->count());
+    }
+
+    public function testListSearchOperation()
+    {
+        $this->bindClient($this->client);
+
+        $entries = $this->client->search(Operations::list(Filters::raw('(&(objectClass=inetOrgPerson)(cn=A*))'), 'ou=Payroll,dc=example,dc=com'));
+        $this->assertInstanceOf(Entries::class, $entries);
+        $this->assertEquals(100, $entries->count());
+
+        /** @var Entry $entry */
+        foreach ($entries as $entry) {
+            $this->assertEquals('ou=Payroll,dc=example,dc=com',$entry->getDn()->getParent()->toString());
+        }
+    }
+
+    public function testWhoAmI()
+    {
+        $this->bindClient($this->client);
+
+        $this->assertEquals('dn:'.$_ENV['LDAP_USERNAME'], $this->client->whoami());
+    }
+
+    public function testStartTls()
+    {
+        $this->client->startTls();
+        $this->assertTrue(true);
+    }
+
+    public function testStartTlsFailure()
+    {
+        $this->client = $this->getClient(['servers' => 'ldap.foo.com']);
+
+        $this->expectException(ConnectionException::class);
+        @$this->client->startTls();
+    }
+
+    public function testStartTlsIgnoreCertValidation()
+    {
+        $this->client = $this->getClient(['servers' => 'ldap.foo.com', 'ssl_validate_cert' => false]);
+
+        $this->client->startTls();
+        $this->assertTrue(true);
+    }
+}
