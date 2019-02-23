c7ef68a39a30a857c76e9b14976cf4896fa9a00e
Chad Sikorra <Chad.Sikorra@gmail.com>
2019-02-23 16:53:15 -0600

    Test cert changes.

--- tests/resources/activedirectory/Install-AD-Step2.ps1
+++ tests/resources/activedirectory/Install-AD-Step2.ps1
@@ -40,8 +40,9 @@ Get-ChildItem cert:\LocalMachine\Root | `
 Get-Certificate `
     -Template KerberosAuthentication `
     -DnsName example.com,$Env:COMPUTERNAME `
+    -SubjectName example.com
     -Confirm:$False `
     -CertStoreLocation cert:\LocalMachine\My
-ldifde -i -f "C:\projects\freedsx-ldap\tests\resources\activedirectory\ldif\tls.ldif"
-certutil -pulse
+#ldifde -i -f "C:\projects\freedsx-ldap\tests\resources\activedirectory\ldif\tls.ldif"
+#certutil -pulse
 
