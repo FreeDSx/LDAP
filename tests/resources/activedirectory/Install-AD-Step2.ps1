744b66758aad4fac24a652bde2c0051e420c83c5
Chad Sikorra <Chad.Sikorra@gmail.com>
2019-02-23 12:12:00 -0600

    Readjust appveyor build.

--- tests/resources/activedirectory/Install-AD-Step2.ps1
+++ tests/resources/activedirectory/Install-AD-Step2.ps1
@@ -7,10 +7,19 @@ Import-Module ServerManager
 
 Add-Content C:\Windows\System32\drivers\etc\hosts "`nfoo.com 127.0.0.1"
 
+Install-WindowsFeature -IncludeManagementTools Adcs-Cert-Authority
+
+ldifde -i -k -f "C:\projects\freedsx-ldap\tests\resources\activedirectory\ldif\data.ldif"
+ldifde -h -i -k -f "C:\projects\freedsx-ldap\tests\resources\activedirectory\ldif\admin.ldif"
+
+$Password = ConvertTo-SecureString -String "P@ssword12345" -AsPlainText -Force
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ('admin@example.com', $Password)
+
 # Installs the EntCA role, which on a DC automagically allows LDAPS / StartTLS
 Install-AdcsCertificationAuthority `
     -CAType EnterpriseRootCa `
     -Confirm:$False `
+    -Credential $Credential `
     -Force
 
 # Enable the recycle bin, can use this for control tests
@@ -19,21 +28,5 @@ Enable-ADOptionalFeature `
     -Scope ForestOrConfigurationSet `
     -Target 'example.com' `
     -Server $env:COMPUTERNAME `
-    -Confirm:$False
-
-ldifde -i -k -f "C:\projects\freedsx-ldap\tests\resources\activedirectory\data.ldif"
-
-# This is to mimic the OpenLDAP CI build admin user.
-$AdminUser = New-ADUser `
-    -Name "admin" `
-    -AccountPassword (ConvertTo-SecureString "P@ssword12345" -AsPlainText -Force) `
-    -DisplayName "admin" `
-    -EmailAddress "admin@example.com" `
-    -Enabled $True `
-    -GivenName "Admin" `
-    -Path "DC=example,DC=com" `
-    -SamAccountName "admin" `
-    -UserPrincipalName "admin@example.com" `
-    -ErrorAction Stop
-Add-ADGroupMember -Identity "Domain Admins" -Members "admin@example.com"
-Add-ADGroupMember -Identity "Enterprise Admins" -Members $Env:COMPUTERNAME
+    -Confirm:$False `
+    -Credential $Credential
