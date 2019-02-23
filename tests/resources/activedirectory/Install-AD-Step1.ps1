b75b52635e068d40af4536b0dc676e2adadaf6b7
Chad Sikorra <Chad.Sikorra@gmail.com>
2019-02-23 09:46:58 -0600

    Use the default domain / forest mode.

--- tests/resources/activedirectory/Install-AD-Step1.ps1
+++ tests/resources/activedirectory/Install-AD-Step1.ps1
@@ -8,10 +8,10 @@ Install-WindowsFeature AD-Domain-Services
 Import-Module ADDSDeployment
 Install-ADDSForest -CreateDnsDelegation:$false `
     -DatabasePath "C:\Windows\NTDS" `
-    -DomainMode "WinThreshold" `
+    -DomainMode "Default" `
     -DomainName "example.com" `
     -DomainNetbiosName "EXAMPLE" `
-    -ForestMode "WinThreshold" `
+    -ForestMode "Default" `
     -InstallDns:$true `
     -LogPath "C:\Windows\NTDS" `
     -NoRebootOnCompletion:$true `
