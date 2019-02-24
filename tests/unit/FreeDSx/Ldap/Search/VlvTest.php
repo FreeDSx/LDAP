5f1a42d461a7f900e1a44177c015ab134e27fd0b
Chad Sikorra <Chad.Sikorra@gmail.com>
2019-02-24 15:53:53 -0600

    Adjust VLV test to be more specific with the filter.

--- tests/unit/FreeDSx/Ldap/Search/VlvTest.php
+++ tests/unit/FreeDSx/Ldap/Search/VlvTest.php
@@ -39,10 +39,10 @@ class VlvTest extends LdapTestCase
         $this->client = $this->getClient();
         $this->bindClient($this->client);
 
-        $this->search = Operations::search(Filters::or(
+        $this->search = Operations::search(Filters::and(Filters::or(
             Filters::equal('objectClass', 'user'),
             Filters::equal('objectClass', 'inetOrgPerson')
-        ), 'sn', 'givenName');
+        ), Filters::startsWith('cn','B')), 'sn', 'givenName');
         $this->vlv = new Vlv($this->client, $this->search, 'sn');
     }
 
@@ -54,7 +54,7 @@ class VlvTest extends LdapTestCase
     public function testVlv()
     {
         $this->assertEquals(101, $this->vlv->getEntries()->count());
-        $this->assertEquals(10001, $this->vlv->listSize());
+        $this->assertEquals(453, $this->vlv->listSize());
         $this->assertEquals(1, $this->vlv->listOffset());
         $this->assertTrue($this->vlv->isAtStartOfList());
 
@@ -62,13 +62,13 @@ class VlvTest extends LdapTestCase
         $this->assertEquals(101, $this->vlv->getEntries()->count());
         $this->assertEquals(101, $this->vlv->listOffset());
 
-        $this->vlv->moveTo(5000);
+        $this->vlv->moveTo(300);
         $this->assertEquals(101, $this->vlv->getEntries()->count());
-        $this->assertEquals(5000, $this->vlv->listOffset());
+        $this->assertEquals(300, $this->vlv->listOffset());
 
         $this->vlv->moveBackward(100);
         $this->assertEquals(101, $this->vlv->getEntries()->count());
-        $this->assertEquals(4900, $this->vlv->listOffset());
+        $this->assertEquals(200, $this->vlv->listOffset());
 
         $this->vlv->moveTo($this->vlv->listSize());
         $this->assertEquals(1, $this->vlv->getEntries()->count());
@@ -82,25 +82,25 @@ class VlvTest extends LdapTestCase
         $this->vlv->moveTo(50);
 
         $this->assertEquals(201, $this->vlv->getEntries()->count());
-        $this->assertEquals(5000, $this->vlv->listOffset());
+        $this->assertEquals(225, $this->vlv->listOffset());
 
         $this->vlv->moveForward(25);
         $this->assertEquals(201, $this->vlv->getEntries()->count());
-        $this->assertEquals(7501, $this->vlv->listOffset());
+        $this->assertEquals(340, $this->vlv->listOffset());
 
         $this->vlv->moveBackward(50);
         $this->assertEquals(201, $this->vlv->getEntries()->count());
-        $this->assertEquals(2500, $this->vlv->listOffset());
+        $this->assertEquals(113, $this->vlv->listOffset());
 
         $this->assertEquals(25, $this->vlv->position());
-        $this->assertEquals(10001, $this->vlv->listSize());
+        $this->assertEquals(453, $this->vlv->listSize());
 
         $this->vlv->moveTo(100);
         $this->assertEquals(101, $this->vlv->getEntries()->count());
         $this->assertTrue($this->vlv->isAtEndOfList());
 
         $this->vlv->moveTo(1);
-        $this->assertEquals(200, $this->vlv->getEntries()->count());
+        $this->assertEquals(105, $this->vlv->getEntries()->count());
         $this->assertTrue($this->vlv->isAtStartOfList());
     }
 }
