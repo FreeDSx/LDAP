55e6fbcdd02617bcbf3fe2a0b7f7d0d789887f41
Chad Sikorra <Chad.Sikorra@gmail.com>
2019-02-24 12:21:38 -0600

    test direct cert path.

--- tests/unit/FreeDSx/Ldap/LdapTestCase.php
+++ tests/unit/FreeDSx/Ldap/LdapTestCase.php
@@ -24,7 +24,8 @@ class LdapTestCase extends TestCase
         $default = [
             'servers' => $_ENV['LDAP_SERVER'],
             'port' => $_ENV['LDAP_PORT'],
-            'ssl_ca_cert' => $_ENV['LDAP_CA_CERT'] === '' ? __DIR__.'/../../../resources/cert/ca.crt' : $_ENV['LDAP_CA_CERT'],
+            // 'ssl_ca_cert' => $_ENV['LDAP_CA_CERT'] === '' ? __DIR__.'/../../../resources/cert/ca.crt' : $_ENV['LDAP_CA_CERT'],
+            'ssl_ca_cert' => 'C:\projects\freedsx-ldap\tests\resources\cert\ca.crt',
             'base_dn' => $_ENV['LDAP_BASE_DN'],
             'ssl_allow_self_signed' => true,
         ];
