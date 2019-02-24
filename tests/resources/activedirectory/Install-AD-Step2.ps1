e571bc967cbc7f161f255ddf94a37a17726495b9
Chad Sikorra <Chad.Sikorra@gmail.com>
2019-02-24 10:10:32 -0600

    Change how the certs are generated.

--- tests/resources/activedirectory/Install-AD-Step2.ps1
+++ tests/resources/activedirectory/Install-AD-Step2.ps1
@@ -7,21 +7,11 @@ Import-Module ServerManager
 
 Add-Content C:\Windows\System32\drivers\etc\hosts "`r`n127.0.0.1 foo.com"
 
-Install-WindowsFeature -IncludeManagementTools Adcs-Cert-Authority
-
 ldifde -i -k -f "C:\projects\freedsx-ldap\tests\resources\activedirectory\ldif\data.ldif"
 ldifde -h -i -k -f "C:\projects\freedsx-ldap\tests\resources\activedirectory\ldif\admin.ldif"
 
 $Password = ConvertTo-SecureString -String "P@ssword12345" -AsPlainText -Force
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ('admin@example.com', $Password)
-
-# Installs the EntCA role, which on a DC automagically allows LDAPS / StartTLS
-Install-AdcsCertificationAuthority `
-    -CAType EnterpriseRootCa `
-    -CACommonName "FreeDSx-CA" `
-    -Confirm:$False `
-    -Credential $Credential `
-    -Force
 
 # Enable the recycle bin, can use this for control tests
 Enable-ADOptionalFeature `
@@ -33,14 +23,13 @@ Enable-ADOptionalFeature `
     -Credential $Credential
 
 New-Item -Path "C:\projects\freedsx-ldap\tests\resources" -Name "cert" -ItemType directory
-Get-ChildItem cert:\LocalMachine\Root | `
-    Where-Object { $_.Subject -match $env:COMPUTERNAME } | `
-    Select-Object -First 1 | `
-    Export-Certificate -FilePath "C:\projects\freedsx-ldap\tests\resources\cert\ca.crt"
 
-certreq.exe -new "C:\projects\freedsx-ldap\tests\resources\activedirectory\cert\cert.inf" "C:\projects\freedsx-ldap\tests\resources\activedirectory\cert\cert.req"
-certreq.exe -submit -config ($Env:COMPUTERNAME + '\FreeDSx-CA') "C:\projects\freedsx-ldap\tests\resources\activedirectory\cert\cert.req" "C:\projects\freedsx-ldap\tests\resources\activedirectory\cert\cert.crt"
-certreq.exe -accept -machine "C:\projects\freedsx-ldap\tests\resources\activedirectory\cert\cert.crt"
+openssl.exe genrsa -out "C:\projects\freedsx-ldap\tests\resources\cert\ca.key" 4096
+openssl.exe req -new -x509 -days 3650 -key "C:\projects\freedsx-ldap\tests\resources\cert\ca.key" -out "C:\projects\freedsx-ldap\tests\resources\cert\ca.crt" -subj "/CN=example.com"
+Import-Certificate -Filepath "C:\projects\freedsx-ldap\tests\resources\cert\ca.crt" -CertStoreLocation cert:\LocalMachine\Root
 
-#certutil.exe -SetCAtemplates +KerberosAuthentication
-#certutil.exe -pulse
+certreq.exe -new "C:\projects\freedsx-ldap\tests\resources\activedirectory\cert\cert.inf" "C:\projects\freedsx-ldap\tests\resources\activedirectory\cert\cert.csr"
+openssl.exe x509 -req -days 3650 -in "C:\projects\freedsx-ldap\tests\resources\activedirectory\cert\cert.csr" -CA "C:\projects\freedsx-ldap\tests\resources\cert\ca.crt" -CAkey "C:\projects\freedsx-ldap\tests\resources\cert\ca.key" -extfile "C:\projects\freedsx-ldap\tests\resources\activedirectory\cert\ext.txt" -set_serial 01 -out "C:\projects\freedsx-ldap\tests\resources\activedirectory\cert\ldap.crt"
+certreq.exe -accept "C:\projects\freedsx-ldap\tests\resources\activedirectory\cert\ldap.crt"
+
+ldifde -i -f "C:\projects\freedsx-ldap\tests\resources\activedirectory\ldif\tls.ldif"
