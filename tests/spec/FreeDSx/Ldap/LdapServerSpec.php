5dc6d77b7b69b3e0de72d89d899986597f4c1094
Chad Sikorra <Chad.Sikorra@gmail.com>
2019-11-30 10:54:31 -0600

    Update the spec.

--- tests/spec/FreeDSx/Ldap/LdapServerSpec.php
+++ tests/spec/FreeDSx/Ldap/LdapServerSpec.php
@@ -17,6 +17,7 @@ use FreeDSx\Ldap\Server\RequestHandler\GenericRequestHandler;
 use FreeDSx\Ldap\Server\RequestHandler\ProxyRequestHandler;
 use FreeDSx\Ldap\Server\ServerRunner\ServerRunnerInterface;
 use FreeDSx\Socket\SocketServer;
+use PhpSpec\Exception\Example\SkippingException;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 
@@ -29,11 +30,19 @@ class LdapServerSpec extends ObjectBehavior
 
     function it_is_initializable()
     {
+        if (!extension_loaded('pcntl')) {
+            throw new SkippingException('The PCNTL extension is required for this spec.');
+        }
+
         $this->shouldHaveType(LdapServer::class);
     }
 
     function it_should_run_the_server($serverRunner)
     {
+        if (!extension_loaded('pcntl')) {
+            throw new SkippingException('The PCNTL extension is required for this spec.');
+        }
+
         $serverRunner->run(Argument::type(SocketServer::class))->shouldBeCalled();
 
         $this->run();
@@ -41,16 +50,28 @@ class LdapServerSpec extends ObjectBehavior
 
     function it_should_not_allow_a_request_handler_as_an_object()
     {
+        if (!extension_loaded('pcntl')) {
+            throw new SkippingException('The PCNTL extension is required for this spec.');
+        }
+
         $this->shouldThrow(RuntimeException::class)->during('__construct', [['request_handler' => new GenericRequestHandler()],]);
     }
 
     function it_should_only_allow_a_request_handler_implementing_request_handler_interface()
     {
+        if (!extension_loaded('pcntl')) {
+            throw new SkippingException('The PCNTL extension is required for this spec.');
+        }
+
         $this->shouldThrow(RuntimeException::class)->during('__construct', [['request_handler' => new Entry('foo')]]);
     }
 
     function it_should_allow_a_request_handler_as_a_string_implementing_request_handler_interface()
     {
+        if (!extension_loaded('pcntl')) {
+            throw new SkippingException('The PCNTL extension is required for this spec.');
+        }
+
         $this->shouldNotThrow(RuntimeException::class)->during('__construct', [['request_handler' => ProxyRequestHandler::class],]);
     }
 }
