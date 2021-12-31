218b021317abfd5d2dbd4e24b454450454438c89
Chad Sikorra <Chad.Sikorra@gmail.com>
2021-12-30 19:25:13 -0600

    Add integration tests for the LDAP server.

---
+++ tests/bin/ldapserver.php
@@ -0,0 +1,133 @@
+<?php
+
+declare(strict_types=1);
+
+use FreeDSx\Ldap\Entry\Entries;
+use FreeDSx\Ldap\Entry\Entry;
+use FreeDSx\Ldap\LdapServer;
+use FreeDSx\Ldap\Operation\Request\AddRequest;
+use FreeDSx\Ldap\Operation\Request\CompareRequest;
+use FreeDSx\Ldap\Operation\Request\DeleteRequest;
+use FreeDSx\Ldap\Operation\Request\ModifyDnRequest;
+use FreeDSx\Ldap\Operation\Request\ModifyRequest;
+use FreeDSx\Ldap\Operation\Request\SearchRequest;
+use FreeDSx\Ldap\Server\RequestContext;
+use FreeDSx\Ldap\Server\RequestHandler\GenericRequestHandler;
+
+require __DIR__ . '/../../vendor/autoload.php';
+
+class LdapServerRequestHandler extends GenericRequestHandler{
+    private $users = [
+        'cn=user,dc=foo,dc=bar' => '12345',
+    ];
+
+    public function bind(string $username, string $password): bool
+    {
+        $this->logRequest(
+            'bind',
+            "username => $username, password => $password"
+        );
+
+        return isset($this->users[$username])
+            && $this->users[$username] === $password;
+    }
+
+    public function add(RequestContext $context, AddRequest $add): void
+    {
+        $attrLog = [];
+        foreach ($add->getEntry()->getAttributes() as $attribute) {
+            $attrLog[] = "{$attribute->getName()} => " . implode(
+                ', ',
+                $attribute->getValues()
+            );
+        }
+        $attrLog = implode(', ', $attrLog);
+
+        $this->logRequest(
+            'add',
+            "dn => {$add->getEntry()->getDn()->toString()}, Attributes: {$attrLog}"
+        );
+    }
+
+    public function compare(RequestContext $context, CompareRequest $compare): bool
+    {
+        $filter = $compare->getFilter();
+
+        $this->logRequest(
+            'compare',
+            "dn => {$compare->getDn()->toString()}, Name => {$filter->getAttribute()}, Value => {$filter->getValue()}"
+        );
+
+        return true;
+    }
+
+    public function delete(RequestContext $context, DeleteRequest $delete): void
+    {
+        $this->logRequest(
+            'delete',
+            "dn => {$delete->getDn()->toString()}"
+        );
+    }
+
+    public function modify(RequestContext $context, ModifyRequest $modify): void
+    {
+        $modLog = [];
+        foreach ($modify->getChanges() as $change) {
+            $attribute = $change->getAttribute();
+            $modLog[] = "({$change->getType()}){$attribute->getName()} => " . implode(
+                ', ',
+                $attribute->getValues()
+            );
+        }
+        $modLog = implode(', ', $modLog);
+
+        $this->logRequest(
+            'modify',
+            "dn => {$modify->getDn()->toString()}, Changes: $modLog"
+        );
+    }
+
+    public function modifyDn(RequestContext $context, ModifyDnRequest $modifyDn): void
+    {
+        $dnLog = 'ParentDn => ' . $modifyDn->getNewParentDn()->toString();
+        $dnLog .= ', ParentRdn => ' . $modifyDn->getNewRdn()->toString();
+
+        $this->logRequest(
+            'modify-dn',
+            "dn => {$modifyDn->getDn()->toString()}, $dnLog"
+        );
+    }
+
+    public function search(RequestContext $context, SearchRequest $search): Entries
+    {
+        $this->logRequest(
+            'search',
+            "base-dn => {$search->getBaseDn()->toString()}, filter => {$search->getFilter()->toString()}"
+        );
+
+        return new Entries(
+            Entry::fromArray(
+                'cn=user,dc=foo,dc=bar',
+                [
+                    'name' => 'user',
+                ]
+            )
+        );
+    }
+
+    private function logRequest(
+        string $type,
+        string $message
+    ): void {
+        echo "---$type--- $message".PHP_EOL;
+    }
+}
+
+$server = new LdapServer([
+    'request_handler' => LdapServerRequestHandler::class,
+    'port' => 3389,
+]);
+
+echo "server starting...".PHP_EOL;
+
+$server->run();
---
+++ tests/unit/FreeDSx/Ldap/LdapServerTest.php
@@ -0,0 +1,347 @@
+<?php
+
+namespace unit\FreeDSx\Ldap;
+
+use Exception;
+use FreeDSx\Ldap\Entry\Entry;
+use FreeDSx\Ldap\Exception\BindException;
+use FreeDSx\Ldap\Exception\OperationException;
+use FreeDSx\Ldap\LdapClient;
+use FreeDSx\Ldap\Operation\ResultCode;
+use Symfony\Component\Process\Process;
+
+class LdapServerTest extends LdapTestCase
+{
+    /**
+     * @var Process
+     */
+    private $subject;
+
+    /**
+     * @var LdapClient
+     */
+    private $client;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->subject = new Process([
+            'php',
+            __DIR__ . '/../../../bin/ldapserver.php'
+        ]);
+        $this->subject->start();
+        $this->waitForServerOutput('server starting...');
+
+        $this->client = new LdapClient([
+            'port' => 3389,
+            'servers' => '127.0.0.1'
+        ]);
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+        $this->client->unbind();
+        $this->client = null;
+        $this->subject->stop();
+    }
+
+    public function testItAcceptsBindWithCorrectCredentials(): void
+    {
+        $response = $this->client->bind(
+            'cn=user,dc=foo,dc=bar',
+            '12345'
+        );
+        $output = $this->waitForServerOutput('---bind---');
+
+        $this->assertNotNull($response);
+        $this->assertStringContainsString(
+            'username => cn=user,dc=foo,dc=bar',
+            $output
+        );
+        $this->assertStringContainsString(
+            'password => 12345',
+            $output
+        );
+    }
+
+
+    public function testItRejectsBindWithIncorrectCredentials(): void
+    {
+        $bindException = null;
+
+        try {
+            $this->client->bind(
+                'cn=fake,dc=foo,dc=bar',
+                'also-fake'
+            );
+        } catch (BindException $exception) {
+            $bindException = $exception;
+        }
+
+        $this->assertNotNull($bindException);
+    }
+
+    public function testItPerformsAnAdd(): void
+    {
+        $this->authenticate();
+        $response = $this->client->create(Entry::fromArray(
+            'cn=meh,dc=foo,dc=bar',
+            [
+                'foo' => 'bar',
+            ]
+        ));
+        $output = $this->waitForServerOutput('---add---');
+
+        $this->assertNotNull($response);
+        $this->assertStringContainsString(
+            'dn => cn=meh,dc=foo,dc=bar',
+            $output
+        );
+        $this->assertStringContainsString(
+            'Attributes: foo => bar',
+            $output
+        );
+    }
+
+    public function testItPerformsDelete(): void
+    {
+        $this->authenticate();
+        $response = $this->client->delete('cn=meh,dc=foo,dc=bar');
+        $output = $this->waitForServerOutput('---delete---');
+
+        $this->assertNotNull($response);
+        $this->assertStringContainsString(
+            'dn => cn=meh,dc=foo,dc=bar',
+            $output
+        );
+    }
+
+    public function testItPerformsModify(): void
+    {
+        $this->authenticate();
+
+        $entry = Entry::fromArray(
+            'cn=meh,dc=foo,dc=bar',
+            [
+                'phone' => '123-456-7890',
+                'email' => 'meh@bleh.local',
+                'surname' => 'oh',
+                'givenName' => 'fake',
+            ]
+        );
+        $entry->add('email', 'foo@bar.local');
+        $entry->remove('givenName', 'FirstName');
+        $entry->set('surname', 'LastName');
+        $entry->reset('phone');
+
+        $response = $this->client->update($entry);
+        $output = $this->waitForServerOutput('---modify---');
+
+        $this->assertNotNull($response);
+        $this->assertStringContainsString(
+            'dn => cn=meh,dc=foo,dc=bar',
+            $output
+        );
+        $this->assertStringContainsString(
+            'Changes: (0)email => foo@bar.local, (1)givenName => FirstName, (2)surname => LastName, (1)phone => ',
+            $output
+        );
+    }
+
+    public function testItPerformsSearches(): void
+    {
+        $this->authenticate();
+
+        $response = $this->client->read('cn=meh,dc=foo,dc=bar');
+        $output = $this->waitForServerOutput('---search---');
+
+        $this->assertNotNull($response);
+        $this->assertStringContainsString(
+            'dn => cn=meh,dc=foo,dc=bar',
+            $output
+        );
+        $this->assertStringContainsString(
+            'filter => (objectClass=*)',
+            $output
+        );
+    }
+
+    public function testItCanPerformCompare(): void
+    {
+        $this->authenticate();
+
+        $response = $this->client->compare(
+            'cn=meh,dc=foo,dc=bar',
+            'foo',
+            'bar'
+        );
+        $output = $this->waitForServerOutput('---compare---');
+
+        $this->assertNotNull($response);
+        $this->assertStringContainsString(
+            'dn => cn=meh,dc=foo,dc=bar',
+            $output
+        );
+        $this->assertStringContainsString(
+            'Name => foo',
+            $output
+        );
+        $this->assertStringContainsString(
+            'Value => bar',
+            $output
+        );
+    }
+
+    public function testItCanModifyDn(): void
+    {
+        $this->authenticate();
+
+        $response = $this->client->move(
+            'cn=meh,dc=foo,dc=bar',
+            'cn=bleh,dc=foo,dc=bar'
+        );
+        $output = $this->waitForServerOutput('---modify-dn---');
+
+        $this->assertNotNull($response);
+        $this->assertStringContainsString(
+            'dn => cn=meh,dc=foo,dc=bar',
+            $output
+        );
+        $this->assertStringContainsString(
+            'ParentDn => cn=bleh,dc=foo,dc=bar',
+            $output
+        );
+        $this->assertStringContainsString(
+            'ParentRdn => cn=meh',
+            $output
+        );
+    }
+
+    public function testItCanRetrieveTheRootDSE()
+    {
+        $rootDse = $this->client->read();
+
+        $this->assertNotNull($rootDse);
+        $this->assertEquals(
+            [
+                'namingContexts' => [
+                    'dc=FreeDSx,dc=local',
+                ],
+                'supportedExtension' => [
+                    '1.3.6.1.4.1.4203.1.11.3',
+                ],
+                'supportedLDAPVersion' => [
+                    '3',
+                ],
+                'vendorName' => [
+                    'FreeDSx',
+                ],
+            ],
+            $rootDse->toArray()
+        );
+    }
+
+    public function testThatOperationCompareRequireAuthentication()
+    {
+        $this->expectException(OperationException::class);
+        $this->expectExceptionCode(ResultCode::INSUFFICIENT_ACCESS_RIGHTS);
+
+        $this->client->compare('dc=foo,dc=bar', 'foo', 'bar');
+    }
+
+    public function testThatOperationSearchRequireAuthentication()
+    {
+        $this->expectException(OperationException::class);
+        $this->expectExceptionCode(ResultCode::INSUFFICIENT_ACCESS_RIGHTS);
+
+        $this->client->read('dc=foo,dc=bar');
+    }
+
+    public function testThatOperationAddRequireAuthentication()
+    {
+        $this->expectException(OperationException::class);
+        $this->expectExceptionCode(ResultCode::INSUFFICIENT_ACCESS_RIGHTS);
+
+        $this->client->create(Entry::fromArray('dc=foo,dc=bar', ['foo' => 'bar']));
+    }
+
+    public function testThatOperationDeleteRequireAuthentication()
+    {
+        $this->expectException(OperationException::class);
+        $this->expectExceptionCode(ResultCode::INSUFFICIENT_ACCESS_RIGHTS);
+
+        $this->client->delete('dc=foo,dc=bar');
+    }
+
+    public function testThatOperationModifyRequireAuthentication()
+    {
+        $this->expectException(OperationException::class);
+        $this->expectExceptionCode(ResultCode::INSUFFICIENT_ACCESS_RIGHTS);
+
+        $entry = Entry::fromArray('cn=foo,dc=foo,dc=bar');
+        $entry->add('email', 'foo@bar.local');
+
+        $this->client->update($entry);
+    }
+
+    public function testThatOperationModifyDnRequireAuthentication()
+    {
+        $this->expectException(OperationException::class);
+        $this->expectExceptionCode(ResultCode::INSUFFICIENT_ACCESS_RIGHTS);
+
+        $this->client->move('dc=foo,dc=bar', 'cn=here, dc=foo, dc=bar');
+    }
+
+    public function testWhoAmIWhenAuthenticated()
+    {
+        $this->authenticate();
+        $output = $this->client->whoami();
+
+        $this->assertEquals('dn:cn=user,dc=foo,dc=bar', $output);
+    }
+
+    public function testWhoAmIWhenNotAuthenticated()
+    {
+        $output = $this->client->whoami();
+
+        $this->assertNull($output);
+    }
+
+    private function authenticate(): void
+    {
+        $this->client->bind(
+            'cn=user,dc=foo,dc=bar',
+            '12345'
+        );
+    }
+
+    private function waitForServerOutput(string $marker): string
+    {
+        $maxWait = 10;
+        $i = 0;
+
+        while ($this->subject->isRunning()) {
+            $output = $this->subject->getOutput();
+            //$this->subject->clearOutput();
+
+            if (strpos($output, $marker) !== false) {
+                return $output;
+            }
+
+            $i++;
+            if ($i === $maxWait) {
+                break;
+            }
+
+            sleep(1);
+        }
+
+        throw new Exception(sprintf(
+            'The expected output (%s) was not received after %d seconds.',
+            $marker,
+            $i
+        ));
+    }
+}
