7e3667c370e534fc9b1af270506ef5f9a2211569
Chad Sikorra <Chad.Sikorra@gmail.com>
2022-05-14 11:02:42 -0500

    Add a simple spec for the child process.

---
+++ tests/spec/FreeDSx/Ldap/Server/ChildProcessSpec.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * This file is part of the FreeDSx LDAP package.
+ *
+ * (c) Chad Sikorra <Chad.Sikorra@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace spec\FreeDSx\Ldap\Server;
+
+use FreeDSx\Ldap\Server\ChildProcess;
+use FreeDSx\Socket\Socket;
+use PhpSpec\ObjectBehavior;
+
+class ChildProcessSpec extends ObjectBehavior
+{
+    public function let(Socket $socket)
+    {
+        $this->beConstructedWith(9001, $socket);
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(ChildProcess::class);
+    }
+
+    public function it_should_get_the_pid()
+    {
+        $this->getPid()->shouldBeEqualTo(9001);
+    }
+
+    public function it_should_get_the_socket()
+    {
+        $this->getSocket()->shouldBeAnInstanceOf(Socket::class);
+    }
+}
