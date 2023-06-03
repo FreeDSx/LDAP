14289f65fdafc999d2c96d7cedc230d677eff629
Chad Sikorra <chad.sikorra@gmail.com>
2023-06-03 12:17:11 -0500

    When constructing an entry from an associative array, explicitly cast any values to strings. This needs to happen during the encoding process anyway. If a consumer of this library needs non-strings in a specific format, they will need to handle that on their own. This just makes what would be implicit explicit.

--- tests/integration/FreeDSx/Ldap/ServerTestCase.php
+++ tests/integration/FreeDSx/Ldap/ServerTestCase.php
@@ -55,7 +55,7 @@ class ServerTestCase extends LdapTestCase
             $output = $this->subject->getOutput();
             $this->subject->clearOutput();
 
-            if (strpos($output, $marker) !== false) {
+            if (str_contains($output, $marker)) {
                 return $output;
             }
 
