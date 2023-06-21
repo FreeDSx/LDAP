aec0aba26ad4dca1bc9528c5236f260f5309560f
Chad Sikorra <chad.sikorra@gmail.com>
2023-06-21 08:41:38 -0500

    Add a sync test for the client methods.

---
+++ tests/integration/FreeDSx/Ldap/Sync/SyncReplTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the FreeDSx LDAP package.
+ *
+ * (c) Chad Sikorra <Chad.Sikorra@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace integration\FreeDSx\Ldap\Sync;
+
+use FreeDSx\Ldap\Sync\Result\SyncEntryResult;
+use integration\FreeDSx\Ldap\LdapTestCase;
+
+class SyncReplTest extends LdapTestCase
+{
+    public function testItCanPerformPollingSync(): void
+    {
+        $entries = [];
+
+        $client = $this->getClient();
+        $this->bindClient($client);
+
+        $client
+            ->syncRepl()
+            ->poll(fn(SyncEntryResult $result) => array_push($entries, $result));
+
+        $this->assertGreaterThan(
+            0,
+            $entries,
+        );
+    }
+}
